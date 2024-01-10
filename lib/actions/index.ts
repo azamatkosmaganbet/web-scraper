6ed44057bce15fc95ab2a84eeedac602276464f4
Azamat <a.kosmaganbet@gmail.com>
2024-01-11 04:55:48 +0500

    Product details page

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,13 +1,78 @@
 "use server";
 
+import Product from "../models/product.model";
+import { revalidatePath } from "next/cache";
+import { connectToDB } from "../mongoose";
 import { scrapeTechnodomProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
-  if (!productUrl) return;
+  if(!productUrl) return;
 
   try {
+    connectToDB();
+
     const scrapedProduct = await scrapeTechnodomProduct(productUrl);
-  } catch (e: any) {
-    throw new Error("Failed to create/update product");
+
+    if(!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice }
+      ]
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+    console.log(newProduct);
+    
+
+    revalidatePath(`/products/${newProduct._id}`);
+  } catch (error: any) {
+    console.log(error);
+  }
+}
+
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+
+    const product = await Product.findOne({ _id: productId });
+
+    if(!product) return null;
+
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
+  } catch (error) {
+    console.log(error);
   }
 }
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import * as cheerio from 'cheerio'
+import * as cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 export async function scrapeTechnodomProduct(url: string) {
   if (!url) return;
 
@@ -22,10 +23,72 @@ export async function scrapeTechnodomProduct(url: string) {
   try {
     const response = await axios.get(url, options);
     const $ = cheerio.load(response.data);
-    const title = $('.title__xmedium').text().trim();
-    console.log({title});
+    const title = $("h1.title__xmedium").text().trim();
+    const currentPrice = extractPrice(
+      $(".product__main-price-info div.product__price")
+    );
+    const originalPrice = extractPrice(
+      $(".usual-price-block div.product__price-old")
+    );
+    const imageUrls: string[] = [];
+    $(".product__gallery img.image-size-cls").each(function () {
+      const src = $(this).attr("src");
+      if (src) {
+        imageUrls.push(src);
+      }
+    });
+
+    const description = extractDescription($);
+    const stars1 = $(
+      "#add-comment div.product__reviews-block-overall-rating-wrapper div.product__reviews-block-overall-rating-value"
+    )
+      .text()
+      .trim();
+    const reviewsCount = $(".product__reviews-block-overall-rating-text")
+      .text()
+      .trim();
+    // const currency = extractCurrency($('.a-price-symbol'))
+    // const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
+
+    const data = {
+      url,
+      currency: "₸",
+      image: imageUrls[0],
+      title,
+      description: description,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(originalPrice) - Number(currentPrice),
+      category: "category",
+      reviewsCount: reviewsCount,
+      stars: Number(stars1),
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    };
+    console.log(data);
     
-  } catch(e: any) {
+    return data;
+  } catch (e: any) {
     console.log(e);
   }
 }
+
+// const title = $("h1.Typography.Typography__XL.Typography__XL_Bold")
+//       .text()
+//       .trim();
+//     const currentPrice = extractPrice(
+//       $(
+//         ".ProductActions_price__OBP_x p.Typography.ProductPrices_accented__pe2rG.Typography__Heading.Typography__Heading_H1"
+//       )
+//     );
+//     const originalPrice = extractPrice(
+//       $(
+//         ".ProductActions_price__OBP_x p.Typography.ProductPrices_oldPrice__e7R8n.Typography__Body.Typography__Body_Strikethrough"
+//       )
+//     );
+//     const images =
+//       $(".Gallery_block__6ZTwh.ProductInfo_gallery__u8lKT img").attr("src") ||
+//       $(".LazyImage__Source").attr("src") ||
+//       "";
